51fea5e3dbd1636e9cf1f2f4a2867e0192e27169
RiseofRice <xthekay@gmail.com>
2020-07-21 21:35:17 +0200

    bLUB

--- app.py
+++ app.py
@@ -94,5 +94,5 @@ def apicheck():
 
 
 if __name__ == "__main__":
-    # TODO Change before push
-    app.run(host='0.0.0.0', port=8080, debug=True)
+
+    app.run(host='0.0.0.0', port=8080, debug=False)
