f4d03caa1589807b31c11364125b4be6fb03e496
RiseofRice <xthekay@gmail.com>
2024-10-07 03:12:36 +0200

    Refactor passwordcheck.py: Add password strength evaluation function

--- passwordcheck.py
+++ passwordcheck.py
@@ -1,4 +1,15 @@
 def check_password(password):
+    """
+    Evaluates the strength of a given password and provides reasons for its strength rating.
+
+    Parameters:
+    password (str): The password to be evaluated.
+
+    Returns:
+    tuple: A tuple containing:
+        - strength (str): The strength of the password ("weak" or "strong").
+        - reasons (list): A list of reasons explaining why the password is considered weak.
+    """
     strength = "weak"
     reasons = []
 
